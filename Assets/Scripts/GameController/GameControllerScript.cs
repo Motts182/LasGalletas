732f475e0311833d045bffa888e8a15ffdecb984
Motts182 <mnd.matias.diaz@gmail.com>
2016-04-17 14:45:45 -0300

    FirstCommit

---
+++ Assets/Scripts/GameController/GameControllerScript.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+
+
+public class GameControllerScript : MonoBehaviour {
+
+    public GameObject canvas;
+    public GameObject[] btns;
+    //crear Enums con todos los nombres de botones y referencias;
+
+    void Awake() {
+       DontDestroyOnLoad(transform.gameObject);
+       btns = GameObject.FindGameObjectsWithTag("btn"); 
+    }
+
+    void Start() {
+
+        StartCoroutine(Test());
+    }
+
+    IEnumerator Test() {
+
+       yield return new WaitForSeconds(5f);
+
+       GameObject footer =  GameObject.Find("Footer");
+        
+       footer.SetActive(false);
+       
+       yield return new WaitForSeconds(1f);
+
+       footer.SetActive(true);
+
+       BuscarBtn("MuroBtn");
+
+       yield return new WaitForSeconds(1f);
+
+       SceneManager.LoadScene("Mapa");
+       DesActiveBtn("MuroBtn");
+
+       yield return new WaitForSeconds(1f);
+
+       ActiveBtn("MuroBtn");
+
+    }
+
+    //Busca Botones por nombre;
+    void BuscarBtn(string btnName) {
+        foreach (GameObject btn in btns) {
+            if (btn.name == btnName) {
+                print(btn.name);
+            }
+        }
+    }
+
+    //Desactivar Botonpor nombre;
+    void DesActiveBtn(string btnName)
+    {
+        foreach (GameObject btn in btns)
+        {
+            if (btn.name == btnName)
+            {
+                print(btn.name);
+                btn.SetActive(false);
+            }
+        }
+    }
+
+    //Activa Botonpor nombre;
+    void ActiveBtn(string btnName)
+    {
+        foreach (GameObject btn in btns)
+        {
+            if (btn.name == btnName)
+            {
+                print(btn.name);
+                btn.SetActive(true);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Slash/SplashScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+
+public class SplashScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(AnimationSplash());
+        print("Start");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator AnimationSplash(){
+        print("Animation");
+
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Muro");
+
+        print("End");
+
+    }
+
+}
